1c8ed4113071a7feecc29d2731aa9dff0e6013cf
Abhay Singh <singhchirag188@gmail.com>
2025-01-10 13:41:48 +0530

    queryies moved to different file

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -3,9 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    //uri: "https://servertaskmaster.onrender.com/api/graphql",
-    //uri: "http://localhost:3000/api/graphql",
-    uri: "https://server-task-master-tt4x-2ihcrnkk1.vercel.app/api/graphql",
+    uri: "http://localhost:3000/api/graphql",
   }),
   cache: new InMemoryCache(),
 });
